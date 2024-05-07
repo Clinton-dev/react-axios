4a34b4ebe4f34012ced7e6c1edfb8371289515d3
clinton-dev <clintonwambugu@gmail.com>
2024-05-07 16:59:59 +0300

    Fix .length property

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Home({ posts }) {
   return (
     <main className="Home">
-      {posts.length ? (
+      {posts?.length ? (
         <Feed posts={posts} />
       ) : (
         <p style={{ marginTop: "2rem" }}>No posts to display.</p>
