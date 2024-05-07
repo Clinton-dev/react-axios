6c1573868a567217afea68024c4443cd72ca8b33
clinton-dev <clintonwambugu@gmail.com>
2024-05-07 16:56:50 +0300

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { Routes, Route } from "react-router-dom";
 
 import Header from "./Header";
 import Footer from "./Footer";
+import Home from "./Home";
+import EditPost from "./EditPost";
+import PostPage from "./PostPage";
 
 function App() {
   return (
@@ -9,8 +12,11 @@ function App() {
       <Header title="my blog" />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/success" element={<Success />} />
+        <Route path="/post" element={<NewPost />} />
+        <Route path="/edit/:id" element={<EditPost />} />
+        <Route path="/post/:id" element={<PostPage />} />
+        <Route path="/about" element={<About />}/>
+        <Route path="*" element={<PostPage />} />
       </Routes>
       <Footer />
     </div>
